0b09f834b1317798163c94d645e3c259acb69404
Samuel Morales <samueldmorales28@gmail.com>
2024-10-06 09:20:42 -0500

    Project done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
+
 function App() {
-  return(
-    <h1>Rick and Morty App</h1>
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const Initialurl = "https://rickandmortyapi.com/api/character";
+
+  const FetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevious = () => {
+    FetchCharacters(info.prev);
+  };
+
+  const onNext = () => {
+    FetchCharacters(info.next);
+  };
+
+  useEffect(() => {
+    FetchCharacters(Initialurl);
+  }, []);
+
+  return (
+    <>
+      <Navbar brand="Rick and Morty App" />
+
+      <div className="container mt-5">
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+        <Characters characters={characters} />
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+      </div>
+    </>
   );
 }
 
